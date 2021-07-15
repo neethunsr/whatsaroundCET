7bad0b11ccc07d70c799a6d21ffc9eefa12c9658
neethunsr <neethu2001nsr@gmail.com>
2021-07-15 13:01:36 +0530

    New files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Logo from './Logo';
+
+function App() {
+  return (
+    <div>Hey</div>
+  );
+}
+
+export default App;
