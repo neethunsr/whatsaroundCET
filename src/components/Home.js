b67b87f4a49e95d9b5fd140c6fe4c417ca1e73ec
neethunsr <neethu2001nsr@gmail.com>
2021-07-19 14:04:15 +0530

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './images/logo.svg'
 import cet from './images/cet-illustration.svg'
-import circle from './images/circles.svg'
+import circle from './images/circles(1).svg'
 import './Home.css'
 function Home(props) {
     return (
@@ -12,19 +12,22 @@ function Home(props) {
                 <img src={logo} className="logo-home" alt="whatsaroundCET"></img>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lectus cursus vitae, felis suspendisse enim sit. Montes, dictum nisi turpis volutpat venenatis facilisis sit eu proin.?</p>
                 </div>
-                <div className="col col-lg-1"></div>
-                <div className="col col-xs-12 col-lg-7">
+                <div className="col col-xs-12 col-lg-8">
                 <img src={cet} className="img-cet" alt="cet"></img>
                 </div>
                 </div>
+                
+                <a href="#" class="btn btn-outline-primary">Subscribe</a>
+                
             </div>
+            
             <section>
             <div className="container align-items-center">
                 <div className="explore">
                 <h3>Explore the world around CET</h3>
                 </div>
                 <hr></hr>
-                <img className='row img-circle' src={circle} alt="circle"></img>
+                <img className='img-circle' src={circle} alt="circle"></img>
                 
             </div>
             </section>
