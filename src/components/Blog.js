fed798d3238d4a46d6cd1a170963d6db611d812d
neethunsr <neethu2001nsr@gmail.com>
2021-07-19 15:25:37 +0530

    form

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,51 +5,53 @@ import './Blog.css'
 function Blog(props) {
     return (
         <div>
-            <div className="blog container">
+            <div className="blog container row">
             <img src={blog} alt="blog-cet"></img>
             <div className="col-8">
-            <div class="card">
-                <div class="card-header">
-                    Harry Potter
+                <div class="card">
+                    <div class="card-header">
+                        Harry Potter
+                    </div>
+                    <div class="card-body">
+                        <h2 class="card-title">Our First Mass Bunk at CET</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare proin interdum faucibus et arcu pellentesque parturient...</p>
+                        <a href="./Home.js" class="btn btn-primary">Read More</a>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <h2 class="card-title">Our First Mass Bunk at CET</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare proin interdum faucibus et arcu pellentesque parturient...</p>
-                    <a href="./Home.js" class="btn btn-primary">Read More</a>
+                <div class="card">
+                    <div class="card-header">
+                        Hermoine Granger
+                    </div>
+                    <div class="card-body">
+                        <h2 class="card-title">Dhwani 18’ ; A Short Reminiscence</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare proin interdum faucibus et arcu pellentesque parturient...</p>
+                        <a href="./Home.js" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        Ron Weasley
+                    </div>
+                    <div class="card-body">
+                        <h2 class="card-title">Blockchain Technology</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare proin interdum faucibus et arcu pellentesque parturient...</p>
+                        <a href="./Home.js" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        Harry Potter
+                    </div>
+                    <div class="card-body">
+                        <h2 class="card-title">Our First Mass Bunk at CET</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare proin interdum faucibus et arcu pellentesque parturient...</p>
+                        <a href="./Home.js" class="btn btn-primary">Read More</a>
+                    </div>
                 </div>
             </div>
-            <div class="card">
-                <div class="card-header">
-                    Hermoine Granger
-                </div>
-                <div class="card-body">
-                    <h2 class="card-title">Dhwani 18’ ; A Short Reminiscence</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare proin interdum faucibus et arcu pellentesque parturient...</p>
-                    <a href="./Home.js" class="btn btn-primary">Read More</a>
-                </div>
+            <div className="add col-4">
+            <img src={add} alt="add-button"></img>
             </div>
-            <div class="card">
-                <div class="card-header">
-                    Ron Weasley
-                </div>
-                <div class="card-body">
-                    <h2 class="card-title">Blockchain Technology</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare proin interdum faucibus et arcu pellentesque parturient...</p>
-                    <a href="./Home.js" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header">
-                    Harry Potter
-                </div>
-                <div class="card-body">
-                    <h2 class="card-title">Our First Mass Bunk at CET</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare proin interdum faucibus et arcu pellentesque parturient...</p>
-                    <a href="./Home.js" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-            </div>
-            <img className="add" src={add} alt="add-button"></img>
             </div>
         </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -5,42 +5,30 @@ function Form(props) {
         <div>
         <form className="row g-3">
         <div className="col-md-6">
-            <label for="inputEmail4" className="form-label">Email</label>
-            <input type="email" class="form-control" id="inputEmail4"></input>
+            <label for="inputEmail4" className="form-label">Title</label>
+            <input type="text" class="form-control" id="inputEmail4"></input>
         </div>
         <div className="col-md-6">
-            <label for="inputPassword4" class="form-label">Password</label>
-            <input type="password" class="form-control" id="inputPassword4"></input>
         </div>
         <div class="col-12">
-            <label for="inputAddress" class="form-label">Address</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"></input>
+            <label for="inputAddress" class="form-label">Blog content</label>
+            <input style={{height: "200px"}} type="text" class="form-control" id="inputAddress" placeholder=""></input>
         </div>
-        <div class="col-12">
-            <label for="inputAddress2" class="form-label">Address 2</label>
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
-        </div>
-        <div class="col-md-6">
-            <label for="inputCity" class="form-label">City</label>
-            <input type="text" class="form-control" id="inputCity"></input>
-        </div>
+    
         <div className="col-md-4">
-            
+            <label for="inputState" class="form-label">Type</label>
+            <select id="inputState" class="form-select">
+            <option selected>Choose...</option>
+            <option>Events</option>
+            <option>Random</option>
+            <option>College Life</option>
+            <option>Technical</option>
+            </select>
         </div>
         <div className="col-md-2">
-            <label for="inputZip" class="form-label">Zip</label>
-            <input type="text" class="form-control" id="inputZip"></input>
         </div>
         <div class="col-12">
-            <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck"></input>
-            <label class="form-check-label" for="gridCheck">
-                Check me out
-            </label>
-            </div>
-        </div>
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary">Sign in</button>
+            <button type="submit" class="btn btn-primary">Add</button>
         </div>
         </form>
         </div>
