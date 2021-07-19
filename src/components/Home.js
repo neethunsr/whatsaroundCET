b90f6e9eaf616b2dc64d2c9f38acab96848a4295
neethunsr <neethu2001nsr@gmail.com>
2021-07-19 11:35:15 +0530

    added bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './images/logo.svg'
 import cet from './images/cet.png'
-import './Home.css'
 function Home(props) {
     return (
         <div>
             <div className="container">
-                <div className="col-6">
+                <div className="row">
+                <div className="col col-lg-6">
                 <img src={logo} className="logo-home" alt="whatsaroundCET"></img>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lectus cursus vitae, felis suspendisse enim sit. Montes, dictum nisi turpis volutpat venenatis facilisis sit eu proin.?</p>
                 </div>
-                <div className="col-6">
+                <div className="col col-lg-6">
                 <img src={cet}></img>
+                </div>
                 </div>
             </div>
             
