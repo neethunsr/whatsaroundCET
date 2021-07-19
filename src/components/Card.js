94a20ab09442ad28108a3117e24a584c02f88d22
Adithyan <adithyanms10@gmail.com>
2021-07-19 13:42:37 +0530

    card cheyth

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,23 @@ import './Card.css';
 
 
 function Card(props) {
+
+    const {name, imgsrc, rs, dis} = props;
     return (
         <div className="card-container">
             <div className="img-container">
-                <img src="/images/k1.jpg" alt="img" width="300" />
+                <img src={imgsrc} alt="img" width="300" height="170" />
             </div>
             <div className="card-title">
-                <h3>Kovalam</h3> 
+                <h3>{name}</h3> 
+            </div>
+            <div className="card-footer">
+                <div className="money">
+                    <h4>Rs {rs}</h4>
+                </div>
+                <div className="dist">
+                    <h4>{dis} Kms</h4>
+                </div>
             </div>
             
         </div>
--- src/components/Travel.js
+++ src/components/Travel.js
@@ -3,6 +3,14 @@ import Card from './Card';
 import './travel.css';
 import logoTravel from './images/logo-travel.svg'
 
+const cardItems = [
+    {name: 'Kovalam', imgsrc: '/images/k1.jpg', rs: '150', dis: '12'},
+    {name: 'Ponmudi', imgsrc: '/images/k2.jpg', rs: '300', dis: '45'},
+    {name: 'Meenmutti', imgsrc: '/images/k3.jpg', rs: '250', dis: '40'}
+    // {name: 'BOOKS', imgsrc: '/books', rs: 'bi bi-book', dis: '12'},
+    // {name: 'FOOD', imgsrc: '/food', rs: 'bi bi-cup-straw', dis: '12'}
+];
+
 function Travel(props) {
     return (
         <div className="travel-container">
@@ -12,8 +20,17 @@ function Travel(props) {
                 </div>
             </div>
             <div className="travel-body">
-            <Card />
-            <Card />
+            {/* <Card name='Kovalam' imgsrc='/images/k1.jpg' rs='200' dis='12' />
+            <Card name='Kovalam' imgsrc='/images/k1.jpg' rs='200' dis='12' /> */}
+            {cardItems.map((cItem, index) => (
+                        <Card 
+                            key={index}
+                            name={cItem.name}
+                            imgsrc={cItem.imgsrc}
+                            rs={cItem.rs}
+                            dis={cItem.dis}
+                        />
+                    ))}
             </div>
             
         </div>
