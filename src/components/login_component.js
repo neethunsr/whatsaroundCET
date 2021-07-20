60c981dc7463c01366f79ac00a4d4a42dcdbe220
psdhanesh7 <psdhanesh2000@gmail.com>
2021-07-20 12:36:22 +0530

    connect login to backend

--- src/components/login_component.js
+++ src/components/login_component.js
@@ -1,39 +1,80 @@
-import React, { Component } from "react";
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import {BrowserRouter as Router,Switch,Route,Link, useHistory} from "react-router-dom";
 import './loginSignup.css'
-export default class Login extends Component {
-    render() {
-        return (
-            <div className="auth-wrapper">
-         <div className="auth-inner">
-         <form>
-                <h3>Sign In</h3>
+export default function Login() {
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+	const history = useHistory();
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-                {/* <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div> */}
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                <a href="#"><Link className="nav-link" to={"/sign-up"}>Sign up</Link></a>
-                </p>
-            </form>
-             </div>
-             </div>
-            
-        );
+        try {
+            const res = await axios.post(
+                'https://whatsaround-backend.herokuapp.com/login',
+                {
+                    mail: email,
+                    password
+                }
+            );
+
+			if(res.data.success) {
+				return history.push('/');
+			}
+
+        } catch(err) {
+            console.log(err.message);
+            alert('Login failed');
+        }
+        
     }
+    
+    return (
+        <div className="auth-wrapper">
+        <div className="auth-inner">
+        <form onSubmit={handleLogin}>
+            <h3>Sign In</h3>
+
+            <div className="form-group">
+                <label>Email address</label>
+                <input 
+                    type="email" 
+                    className="form-control" 
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                />
+            </div>
+
+            <div className="form-group">
+                <label>Password</label>
+                <input 
+                    type="password" 
+                    className="form-control" 
+                    placeholder="Enter password"
+                    value={password}
+                    onChange={(e)=>{setPassword(e.target.value)}}
+                />
+            </div>
+
+            {/* <div className="form-group">
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                </div>
+            </div> */}
+
+            <button type="submit" className="btn btn-primary btn-block">Submit</button>
+            <p className="forgot-password text-right">
+            <a href="#"><Link className="nav-link" to={"/sign-up"}>Sign up</Link></a>
+            </p>
+        </form>
+            </div>
+            </div>
+        
+    );
+    
 }
